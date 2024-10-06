b8c8416c764d64a12f3295fbff70c55554f4cdf9
Tyrone E <tyronee1281@gmail.com>
2024-10-05 20:23:03 -0400

    Added initial functions to traverse basepath and get all file details

--- src/DuplicateFiles_Checker/DuplicateFiles_Main.py
+++ src/DuplicateFiles_Checker/DuplicateFiles_Main.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
-#Base/Main file for Duplicate Checker program
+#Base/Main file for Duplicate File Checker program
+import os
+import hashlib
 
+def getMd5Sum(AbsFName):
+  with open(AbsFName,'rb') as file_to_check:
+    data = file_to_check.read()
+  return hashlib.md5(data).hexdigest()
 
+def getAllFilesDetails(ipath):
+  for root, dirs, files in os.walk(ipath, topdown=True, followlinks=False):
+    for FileName in files:
+      print(root, FileName, os.path.getsize(os.path.join(root,FileName)), getMd5Sum(os.path.join(root,FileName)))
+  
+def main():
+  basepath = '/Users/tyroneestacio/git/'
+  getAllFilesDetails(basepath)
+
+if __name__ == '__main__':
+  main()
