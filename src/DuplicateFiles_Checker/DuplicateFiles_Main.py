00dde0f0608d13c5507226ac5c60d3e47ddf4f0b
Tyrone E <tyronee1281@gmail.com>
2024-10-08 22:04:59 -0400

    Added function to check exact duplicate on content

--- src/DuplicateFiles_Checker/DuplicateFiles_Main.py
+++ src/DuplicateFiles_Checker/DuplicateFiles_Main.py
@@ -5,8 +5,10 @@ import os
 import hashlib
 import pandas as pd
 
-ResFName = 'Results.csv'
-ErrFName = 'Error.csv'
+ResFName = '/tmp/Results.csv'
+ResDupFName = '/tmp/Results_DupName.csv'
+ResDupFSize = '/tmp/Results_DupSize.csv'
+ErrFName = '/tmp/Error.csv'
 
 #=================================================================================================
 def getMd5Sum(AbsFName):
@@ -15,18 +17,21 @@ def getMd5Sum(AbsFName):
   return hashlib.md5(data).hexdigest()
 
 #=================================================================================================
-def getAllFilesDetails(ipath):
+def getInitialFileDetails(ipath):
   ResFile = open(ResFName, 'w')
   ErrFile = open(ErrFName, 'w')
 
   ResFile.write(','.join(['FileName', 'Location', 'FileSize']) + '\n')
   for root, dirs, files in os.walk(ipath, topdown=True, followlinks=False):
     for FileName in files:
-      AbsFName = os.path.join(root,FileName)
-      if (AbsFName,os.R_OK):
-        ResFile.write(','.join([FileName, root, str(os.path.getsize(AbsFName))]) + '\n')
+      if (FileName == ResFName):
+        pass
       else:
-        ErrFile.write(AbsFName + ',' + 'Not Authorized')
+        AbsFName = os.path.join(root,FileName)
+        if (AbsFName,os.R_OK):
+          ResFile.write(','.join([FileName, root, str(os.path.getsize(AbsFName))]) + '\n')
+        else:
+          ErrFile.write(AbsFName + ',' + 'Not Authorized')
   
   ResFile.close()
   ErrFile.close()
@@ -34,32 +39,50 @@ def getAllFilesDetails(ipath):
   return 0
   
 #=================================================================================================
-def getDuplicates(iFile,colCheck):
+def getInitialDups(iFile,colCheck):
   df = pd.read_csv(iFile)
   dfDupRecords = df[df[[colCheck]].duplicated(keep=False)]
 
-  for index, row in dfDupRecords.iterrows():
-    print(dfDupRecords['FileName'][index], 
-          dfDupRecords['Location'][index], 
-          dfDupRecords['FileSize'][index], 
-          getMd5Sum(os.path.join(dfDupRecords['Location'][index],dfDupRecords['FileName'][index])))
+  if (len(dfDupRecords) > 0):
+    OutDup = open('/tmp/Dup'+ colCheck + '.csv', 'w')
+    OutDup.write('FileName,Location,FileSize,md5Sum\n')
+    for index, row in dfDupRecords.iterrows():
+      OutDup.write(str(dfDupRecords['FileName'][index]) + ',' +
+                   str(dfDupRecords['Location'][index]) + ',' +
+                   str(dfDupRecords['FileSize'][index]) + ',' +
+                   str(getMd5Sum(os.path.join(dfDupRecords['Location'][index],dfDupRecords['FileName'][index]))) + '\n')
+    OutDup.close()
   
   return 0 
+
+#=================================================================================================
+def getDuplicateContents(iFile):
+  df = pd.read_csv(iFile)
+  dfDupRecords = df[df[['md5Sum']].duplicated(keep=False)]
+  if (len(dfDupRecords) > 0):
+    ContentDup = open('/tmp/DupContent.csv','w')
+    ContentDup.write('FileName,Location,FileSize,md5Sum\n')
+    for index, row in dfDupRecords.iterrows():
+      ContentDup.write(str(dfDupRecords['FileName'][index]) + ',' +
+                   str(dfDupRecords['Location'][index]) + ',' +
+                   str(dfDupRecords['FileSize'][index]) + ',' +
+                   str(getMd5Sum(os.path.join(dfDupRecords['Location'][index],dfDupRecords['FileName'][index]))) + '\n')
+      #ContentDup.write(dfDupRecords[index])
+  
+    ContentDup.close()
+  return 0
 
 #=================================================================================================
 def main():
   basepath = '/Users/tyroneestacio/git/py_repo/src/'
 
-  getAllFilesDetails(basepath)
-  getDuplicates(ResFName,'FileSize')
-  getDuplicates(ResFName,'FileName')
-
+  getInitialFileDetails(basepath)
+  getInitialDups(ResFName,'FileSize')
+  getInitialDups(ResFName,'FileName')
+  getDuplicateContents('/tmp/DupFileSize.csv')
   return 0
 
 #=================================================================================================
 if __name__ == '__main__':
   main()
-
-
-
 
